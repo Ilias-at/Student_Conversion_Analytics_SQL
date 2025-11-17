1c7bb744827303f42dedd27f8eb5c48e27b8e0ba
ILIAS AT <iliasatb01@gmail.com>
2025-11-17 12:32:08 +0100

    Add files via upload
    - Free-to-Paid Conversion Rate
    - Average Registration → First Watch duration
    - Average First Watch → First Purchase duration
    
    This script includes:
    - Subqueries for earliest engagement/purchase
    - KPI calculations (CR, Avg durations)
    - Data validation logic (watch ≤ purchase)

---
+++ 01 CALCULATION Free-to-Paid Conversion Rate.sql
@@ -0,0 +1,32 @@
+SELECT 
+    ROUND(100 * SUM(CASE
+                WHEN first_date_purchased IS NOT NULL THEN 1
+                ELSE 0
+            END) / COUNT(*),
+            2) AS Conversion_Rate
+from( select
+    si.student_id,
+    si.date_registered,
+    se.first_date_watched,
+    sp.first_date_purchased,
+    DATEDIFF(se.first_date_watched,
+            si.date_registered) AS Date_Diff_Reg_Watch,
+    DATEDIFF(sp.first_date_purchased,
+            se.first_date_watched) AS Date_Diff_Watch_Purch
+FROM
+    student_info si
+        LEFT JOIN
+    (SELECT 
+        student_id, MIN(date_watched) AS First_date_watched
+    FROM
+        student_engagement
+    GROUP BY student_id) se ON si.student_id = se.student_id
+        LEFT JOIN
+    (SELECT 
+        student_id, MIN(date_purchased) AS first_date_purchased
+    FROM
+        student_purchases
+    GROUP BY student_id) sp ON si.student_id = sp.student_id
+WHERE se.first_date_watched IS NOT NULL 
+  AND (sp.first_date_purchased IS NULL OR se.first_date_watched <= sp.first_date_purchased)
+  )as sub 
